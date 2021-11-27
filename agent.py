fce2e421527a5e3c0b77c464774dcca20c4a0248
Michał Kalinowski <44393752+KalinZeglarz@users.noreply.github.com>
2021-11-27 01:04:06 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import random
 from mesa import Agent
 
 
@@ -16,67 +17,57 @@ class LightcycleAgent(Agent):
         super().__init__(pos, model)
         self.unique_id = unique_id
         self.pos = pos
-        self.lightpath = [(-1, n) for n in range(0, 27)] + [(26, n) for n in range(0, 27)] + \
+        self.lightpath = []
+        self.boundries = [(-1, n) for n in range(0, 27)] + [(26, n) for n in range(0, 27)] + \
                          [(n, -1) for n in range(0, 27)] + [(n, 26) for n in range(0, 27)]
         self.direction = direction
+        self.first_move = True
 
     def move(self, fillings):
+        new_direction = ''
         new_pos = self.pos
         while len(fillings) > 0:
-            new_direction = min(fillings, key=fillings.get)
+            if self.first_move:
+                new_direction = random.choice(list(fillings.keys()))
+                self.first_move = False
+            else:
+                new_direction = min(fillings, key=fillings.get)
             new_pos = list(self.pos)
-
             if new_direction == 'N':
                 new_pos[1] += 1
-                if tuple(new_pos) in self.lightpath:
+                if tuple(new_pos) in self.lightpath or tuple(new_pos) in self.boundries:
                     del fillings[new_direction]
                 else:
-                    print("nowa", new_pos, "stara", self.pos)
-                    print("nowy dir", new_direction, "stary dir", self.direction)
-                    self.model.grid.move_agent(self, tuple(new_pos))
-                    self.direction = new_direction
-                    self.pos = tuple(new_pos)
                     break
 
             elif new_direction == 'S':
                 new_pos[1] -= 1
-                if tuple(new_pos) in self.lightpath:
+                if tuple(new_pos) in self.lightpath or tuple(new_pos) in self.boundries:
                     del fillings[new_direction]
                 else:
-                    print("nowa", new_pos, "stara", self.pos)
-                    print("nowy dir", new_direction, "stary dir", self.direction)
-                    self.model.grid.move_agent(self, tuple(new_pos))
-                    self.direction = new_direction
-                    self.pos = tuple(new_pos)
                     break
 
             elif new_direction == 'W':
                 new_pos[0] -= 1
-                if tuple(new_pos) in self.lightpath:
+                if tuple(new_pos) in self.lightpath or tuple(new_pos) in self.boundries:
                     del fillings[new_direction]
                 else:
-                    print("nowa", new_pos, "stara", self.pos)
-                    print("nowy dir", new_direction, "stary dir", self.direction)
-                    self.model.grid.move_agent(self, tuple(new_pos))
-                    self.direction = new_direction
-                    self.pos = tuple(new_pos)
                     break
 
             elif new_direction == 'E':
                 new_pos[0] += 1
-                if tuple(new_pos) in self.lightpath:
+                if tuple(new_pos) in self.lightpath or tuple(new_pos) in self.boundries:
                     del fillings[new_direction]
                 else:
-                    print("nowa", new_pos, "stara", self.pos)
-                    print("nowy dir", new_direction, "stary dir", self.direction)
-                    self.model.grid.move_agent(self, tuple(new_pos))
-                    self.direction = new_direction
-                    self.pos = tuple(new_pos)
                     break
 
         if new_pos == self.pos:
             self.model.grid._remove_agent(self.pos, self)
             self.model.schedule.remove(self)
+        else:
+            self.model.grid.place_agent(self, tuple(new_pos))
+            self.direction = new_direction
+            self.pos = tuple(new_pos)
 
     def step(self):
         self.lightpath.append(self.pos)
