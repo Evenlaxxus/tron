a5e86ce14726cfa6ca75213d855ee21a763e52cd
KalinZeglarz <44393752+KalinZeglarz@users.noreply.github.com>
2021-11-28 21:24:03 +0100

    FoV fix

--- server.py
+++ server.py
@@ -110,7 +110,7 @@ server = ModularServer(TronModel,
                        {
                            "n_agents": UserSettableParameter("slider", "Number of Agents", 4, 2, 12, 1),
                            "max_path_length": UserSettableParameter("slider", "Max Lightpath Length", 676, 10, 676, 1),
-                           "fov": UserSettableParameter("slider", "Field of View", 676, 1, 676, 1),
+                           "fov": UserSettableParameter("slider", "Field of View", 26, 1, 26, 1),
                            "isStartingPositionRandom": UserSettableParameter("checkbox", "Random Starting Positions", False)
                        }
                        )
